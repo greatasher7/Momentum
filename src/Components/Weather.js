4bb259d1fbc5a7ed5bf6149de24d0f12dae4d172
asher <greatasher7@gmail.com>
2021-06-29 15:00:37 +0900

    using react-geocode for getting rigion data / figuring out the useEffect and solve the problem about it

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -1,8 +1,10 @@
 import {useState, useEffect} from "react";
 import axios from "axios";
+import Geocode from "react-geocode";
 import styled from "styled-components";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBolt, faCloud, faSun, faUmbrella, faCloudShowersHeavy, faSnowflake } from '@fortawesome/free-solid-svg-icons'
+
 
 
 const Container = styled.section`
@@ -53,38 +55,74 @@ const filterWeather = (weather) => {
 
 const Weather = () => {
 
-    const [weatherData, setWeatherData] = useState();
     const [locationData, setLocationData] = useState();
-
-    const getLocation = () => {
-        navigator.geolocation.getCurrentPosition( ({coords}) => {
-            let {latitude, longitude} = coords;
-            setLocationData({lat: String(latitude), lon: String(longitude)});
-        });
-    }
-
-    const getWeather = async () => {
-        if(locationData){
-            console.log("true!");
-            try{
-                const {data} = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${"37.2546"}&lon=${"120.4564"}&units=metric&appid=b17f3e894d8bf77f4e9b87e1a0ba390f`);
-                setWeatherData(data);
-            } catch (e){
-                console.log(e)
-            }
-        }
-    }
+    const [weatherData, setWeatherData] = useState();
+    const [regionData, setRegionDate] = useState();
 
     useEffect(()=>{
+        const getLocation = () => {
+            navigator.geolocation.getCurrentPosition( ({coords}) => {
+                let {latitude, longitude} = coords;
+                setLocationData({lat: String(latitude), lon: String(longitude)});
+            });
+        }
         getLocation();
-        getWeather();
     }, []);
+
+
+    useEffect(()=>{
+        const getWeather = async () => {
+            if(locationData){
+                try{
+                    const {data} = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${locationData.lat}&lon=${locationData.lon}&units=metric&appid=b17f3e894d8bf77f4e9b87e1a0ba390f`);
+                    setWeatherData(data);
+                } catch (e){
+                    console.log(e)
+                }
+            }
+        }
+        getWeather();
+    }, [locationData]);
+
+
+    useEffect(()=> {
+        const getRegion = async () => {
+            Geocode.setApiKey('AIzaSyCMI38z86E0qDvgH6zo42Txxzzv60YRegY');
+            if(locationData){
+                try{
+                    const {results} = await Geocode.fromLatLng(locationData.lat, locationData.lon);
+                    let country, city;
+                    for(let i = 0; i< results[0].address_components.length; i++){
+                        for(let j = 0; j < results[0].address_components[i].types.length; j++){
+                            switch (results[0].address_components[i].types[j]){
+                                case "locality":
+                                    city = results[0].address_components[i].long_name;
+                                    break;
+                                case "country":
+                                    country = results[0].address_components[i].long_name;
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+                    }
+                    setRegionDate({
+                        city, country
+                    })
+                } catch (e){
+                    console.log(e);
+                }
+            }
+        }
+        getRegion();
+
+    }, [locationData]);
 
     return(
         <Container>
-            <WeatherImg weather={weatherData ? weatherData.weather[0].main : ""}><FontAwesomeIcon icon={weatherData ? filterWeather(weatherData.weather[0].main) : ""} /></WeatherImg>
+            <WeatherImg weather={weatherData ? weatherData.weather[0].main : ""}><FontAwesomeIcon icon={weatherData ? filterWeather(weatherData.weather[0].main) : faSun} /></WeatherImg>
             <Temperature>{weatherData ? Math.round(Number(weatherData.main.temp)*10)/10 : ""}°</Temperature>
-            <City>Suwon, Korea</City>
+            <City>{regionData ? `${regionData.city}, ${regionData.country}` : ""}</City>
         </Container>
     );
 }
